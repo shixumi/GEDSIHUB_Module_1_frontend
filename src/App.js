8dec8c4b07e9dfbb2ea149dbd92a49e397d4a3c4
Alexander Porlares <92439887+Rednaxela5@users.noreply.github.com>
2024-08-14 18:21:52 +0800

    register testing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import './bootstrap/css/bootstrap.min.css';
+// import './css/bs-theme-overrides.css';
+import './css/activation.css';
+// import './css/Login-Form-Basic-icons.css';
 
 const App = () => {
   const isAuthenticated = !!localStorage.getItem('token');
