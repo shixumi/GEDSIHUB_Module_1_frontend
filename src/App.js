c20e2f39efdb704b57ec1708d7f31635bee11295
shixumi <ldenzell88@gmail.com>
2024-08-14 20:39:15 +0800

    Activation Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import ActivationPage from './pages/ActivationPage';
 import './bootstrap/css/bootstrap.min.css';
 import './css/activation.css';
 // import './css/bs-theme-overrides.css';
@@ -16,6 +17,7 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/activate/:uid/:token" element={<ActivationPage />} />
         <Route
           path="/"
           element={isAuthenticated ? <h1>Home Page</h1> : <Navigate to="/login" />}
